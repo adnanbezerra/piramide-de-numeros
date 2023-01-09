e75fc48756384bd4ba8002eca9009f228addb8a4
seu nome <adnanbezerra@gmail.com>
2023-01-09 20:46:19 -0300

    finish exercise

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Qual o tamanho da sua pirâmide?");
+        int tamanho = s.nextInt();
+        s.close();
+
+        for (int i = 0; i <= tamanho; i++) {
+            StringBuilder linha = new StringBuilder();
+            for(int j = 0; j < i; j++) {
+                linha.append(i);
+            }
+            System.out.println(linha);
+        }
+    }
+}
